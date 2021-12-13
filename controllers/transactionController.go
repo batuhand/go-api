d0a79e5046e14c8cea0db2e547d172af2d08d1cd
batuhand <batuhandemircann@gmail.com>
2021-12-13 22:23:51 +0300

    muxed

--- controllers/transactionController.go
+++ controllers/transactionController.go
@@ -12,50 +12,36 @@ import (
 
 // Returns all the transactions
 func GetAllTransactions(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		transactions, err := json.Marshal(models.DummyTransactions)
-		CheckError(err)
-		fmt.Println(string(transactions))
-		fmt.Fprint(w, string(transactions))
-	} else {
-		fmt.Fprintf(w, "Bad Request")
-	}
+	transactions, err := json.Marshal(models.DummyTransactions)
+	CheckError(err)
+	fmt.Println(string(transactions))
+	fmt.Fprint(w, string(transactions))
+
 }
 
 // Creates a transaction if there are enough currency in the sender wallet adress
 func SendCoin(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		CheckError(err)
-		transaction := models.Transaction{}
-		json.Unmarshal(body, &transaction)
-		senderCurrency := GetWalletAmount(transaction.SenderWalletAdress)
-		if senderCurrency > transaction.Amount {
-			models.DummyTransactions = append(models.DummyTransactions, transaction)
-			//CheckError(err)
-			fmt.Fprint(w, "Transaction created")
-		} else {
-			fmt.Fprintf(w, "Transaction failed, not enough currency")
-		}
-
+	body, err := ioutil.ReadAll(r.Body)
+	CheckError(err)
+	transaction := models.Transaction{}
+	json.Unmarshal(body, &transaction)
+	senderCurrency := GetWalletAmount(transaction.SenderWalletAdress)
+	if senderCurrency > transaction.Amount {
+		models.DummyTransactions = append(models.DummyTransactions, transaction)
+		//CheckError(err)
+		fmt.Fprint(w, "Transaction created")
 	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Bad Request")
+		fmt.Fprintf(w, "Transaction failed, not enough currency")
 	}
 
 }
 
 // Returns the currency of the given wallet adress
 func GetCurrency(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		url_list := strings.Split(r.URL.Path, "/")
-		wallet_adress := url_list[4]
-		currency := GetWalletAmount(wallet_adress)
-		fmt.Fprintf(w, "%f", currency)
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Bad Request")
-	}
+	url_list := strings.Split(r.URL.Path, "/")
+	wallet_adress := url_list[4]
+	currency := GetWalletAmount(wallet_adress)
+	fmt.Fprintf(w, "%f", currency)
 
 }
 
--- main.go
+++ main.go
@@ -4,48 +4,50 @@ import (
 	"net/http"
 
 	"batuhand.com/api/controllers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	rootName := "/api"
-	defer http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
 
 	// ------------------------- USER ENDPOINTS -------------------------
 
 	// Get all users
 	// Method: GET - /get_users/
-	http.HandleFunc(rootName+"/user/get_users/", controllers.GetAllUsers)
+	router.HandleFunc(rootName+"/user/get_users/", controllers.GetAllUsers).Methods("GET")
 
 	// Get user by id
 	// Method: GET - /user/%id
-	http.HandleFunc(rootName+"/user/get_user/", controllers.GetUser)
+	router.HandleFunc(rootName+"/user/get_user/", controllers.GetUser).Methods("GET")
 
 	// Update user by id
 	// Method: PUT - /user/%id
-	http.HandleFunc(rootName+"/user/update_user/", controllers.UpdateUser)
+	router.HandleFunc(rootName+"/user/update_user/", controllers.UpdateUser).Methods("PUT")
 
 	// Delete user by id
 	// Method: DELETE - /user/%id
-	http.HandleFunc(rootName+"/user/delete_user/", controllers.DeleteUser)
+	router.HandleFunc(rootName+"/user/delete_user/", controllers.DeleteUser).Methods("DELETE")
 
 	// New user
 	// Method: POST - /create_user/
-	http.HandleFunc(rootName+"/user/create_user/", controllers.CreateUser)
-
+	router.HandleFunc(rootName+"/user/create_user/", controllers.CreateUser).Methods("POST")
 
 	// ------------------------- TRANSACTION ENDPOINTS -------------------------
 
 	// Get all transactions
 	// Method: GET - /get_transactions/
-	http.HandleFunc(rootName+"/transaction/get_transactions/", controllers.GetAllTransactions)
+	router.HandleFunc(rootName+"/transaction/get_transactions/", controllers.GetAllTransactions).Methods("GET")
 
 	// New transaction
 	// Method: POST - /send_coin/
-	http.HandleFunc(rootName+"/transaction/send_coin/", controllers.SendCoin)
+	router.HandleFunc(rootName+"/transaction/send_coin/", controllers.SendCoin).Methods("POST")
 
 	// Get wallet currency
 	// Method: GET - /get_wallet_currency/
-	http.HandleFunc(rootName+"/transaction/get_wallet_currency/", controllers.GetCurrency)
+	router.HandleFunc(rootName+"/transaction/get_wallet_currency/", controllers.GetCurrency).Methods("GET")
+
+	http.ListenAndServe(":8080", router)
 
 }
